9e60e42b3e62eedd592f51301dae05dea7f0f8eb
BogdanSch <68709795+BogdanSch@users.noreply.github.com>
2021-05-04 18:04:43 +0300

    Add files via upload

---
+++ Prediction/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Training
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bStart = new System.Windows.Forms.Button();
+            this.lRes = new System.Windows.Forms.Label();
+            this.pBV = new System.Windows.Forms.ProgressBar();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bStart
+            // 
+            this.bStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bStart.Location = new System.Drawing.Point(3, 198);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(339, 72);
+            this.bStart.TabIndex = 0;
+            this.bStart.Text = "Предсказать";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // lRes
+            // 
+            this.lRes.AutoSize = true;
+            this.lRes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lRes.Location = new System.Drawing.Point(3, 0);
+            this.lRes.Name = "lRes";
+            this.lRes.Size = new System.Drawing.Size(339, 123);
+            this.lRes.TabIndex = 1;
+            this.lRes.Text = "Предскажу ваше будующее";
+            this.lRes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pBV
+            // 
+            this.pBV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pBV.Location = new System.Drawing.Point(3, 126);
+            this.pBV.Name = "pBV";
+            this.pBV.Size = new System.Drawing.Size(339, 66);
+            this.pBV.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.lRes, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pBV, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.bStart, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 43.90834F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.81989F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30.27177F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(345, 282);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 282);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Predictions";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Label lRes;
+        private System.Windows.Forms.ProgressBar pBV;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ Prediction/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace Training
+{
+    public partial class Form1 : Form
+    {
+        private string[] predictions = new string[14];
+        private Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadPredictions(StreamReader reader)
+        {
+            for (int i = 0; i < predictions.Length; i++)
+            {
+                predictions[i] = reader.ReadLine();
+            }
+            reader.Close();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i <= 100; i++)
+            {
+                pBV.Value = i;
+                Thread.Sleep(10);
+            }
+            MakePrediction();
+
+        }
+
+        private void MakePrediction()
+        {
+            string pred = GetPrediction();
+            MessageBox.Show(pred, "Предсказание");
+        }
+
+        private string GetPrediction()
+        {
+            StreamReader reader = new StreamReader("Examples.txt");
+
+            ReadPredictions(reader);
+
+            int whatElement = rnd.Next(0, predictions.Length - 1);
+
+            return predictions[whatElement];
+        }
+    }
+}
